275424fe5b55a2715b922d23248e04f274a28565
Gustav Brodin <brodin.gustav@gmail.com>
2025-04-09 11:20:12 +0200

    Commit - Inititerat Angular-projekt - Skapat komponenter - Initierat rutter i routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '404', component: NotFoundComponent},
+  {path: '**', component: NotFoundComponent},
+
+
+];
