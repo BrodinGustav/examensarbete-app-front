408114653229c843f3f1da6631591b44b626fbf8
Gustav Brodin <brodin.gustav@gmail.com>
2025-04-09 16:16:15 +0200

    Commit - Implementerat logik för att visa topp 3 användare på startsidan.
    - HomeComponent hämtar nu data från /api/useractivities, sorterar användarna efter poäng och visar de tre bästa under respektive platsikon.
    
    - Användarnamn skrivs ut för första, andra och tredje plats.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LeaderBoardComponent } from '../leader-board/leader-board.component';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,24 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  points: any[] = [];
+  topThreeUsers: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('http://localhost:8080/api/useractivities')
+.subscribe(data => {
+  //Sorterar efter poäng
+  const sorted = data.sort((a, b) => b.activity.points - a.activity.points);
+
+  //Sparar listan
+  this.points = sorted;
+
+  //Sparar tre högsta poäng
+  this.topThreeUsers = sorted.slice(0, 3);
+})
+  }
 
 }
