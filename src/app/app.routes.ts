4cdad92ff092467c3a99300442664587889b7464
Gustav Brodin <brodin.gustav@gmail.com>
2025-04-10 10:51:28 +0200

    Commit - Implementerat registreringsformulär med validering och styling.
    - Skapat ett användarvänligt registreringsformulär med fält för användarnamn, e-post och lösenord.
    
    - Använt Reactive Forms för att hantera formulärdata och validering.
    
    - Lagt till grundläggande CSS-styling för att förbättra formulärets utseende och användarupplevelse.
    
    - Felmeddelanden visas vid ogiltig input och knappen inaktiveras vid ogiltigt formulär.
    
    OBS! Formulär ännu ej kopplat till API!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LogInComponent},
   {path: 'profile', component: ProfileComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/register', pathMatch: 'full'},
   {path: '404', component: NotFoundComponent},
   {path: '**', component: NotFoundComponent},
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', Validators.required),
+    });
+  }
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Godkänd registrering!', this.registerForm.value);
+    }else {
+      console.log('Registrering misslyckades');
+    }
+  }
 
 }
