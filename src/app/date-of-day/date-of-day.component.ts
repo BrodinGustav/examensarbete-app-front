98e8160e57f2f9c80bbe7dc092f718585abc448c
Gustav Brodin <brodin.gustav@gmail.com>
2025-04-10 09:06:07 +0200

    Commit - Implementera live-uppdatering av klocka i DateOfDayComponent
    - Lagt till RxJS `interval` för att skapa en timer som uppdaterar klockan varje sekund.
    
    - `loadTimeAndDate` från `DateOfDayService` anropas varje sekund för att hämta och uppdatera aktuell tid.
    
    - Komponentens vy uppdateras kontinuerligt utan att sidan behöver laddas om.

--- src/app/date-of-day/date-of-day.component.ts
+++ src/app/date-of-day/date-of-day.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DateOfDayService } from '../services/date-of-day.service';
 import { CommonModule, DatePipe } from '@angular/common';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-date-of-day',
@@ -10,19 +11,35 @@ import { CommonModule, DatePipe } from '@angular/common';
   templateUrl: './date-of-day.component.html',
   styleUrl: './date-of-day.component.css'
 })
-export class DateOfDayComponent implements OnInit {
+export class DateOfDayComponent implements OnInit, OnDestroy {
 
 currentDateAndTime: string | null = null;
+private timerSubscription: Subscription | undefined;
+
+
 
   constructor(private dateService: DateOfDayService) { }
 
 
   ngOnInit(): void {
     this.loadTimeAndDate();
+    this.startTimer();
+  }
+
+  ngOnDestroy(): void {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe(); //Stopar interval-subscribe när komponenten tas bort
+    }
   }
 
   loadTimeAndDate(): void {
     this.currentDateAndTime = this.dateService.loadTimeAndDate();
   }
+
+  startTimer(): void {
+    this.timerSubscription = interval(1000).subscribe(() => {     //Intervall skapar en observable som uppdaterar värde varje sekund. Subscribe kör funktionen varje gång nytt värde skickas (varje sekund)
+    this.loadTimeAndDate();                                       //Uppdaterar tiden varje sekund genom interval och subscribe
+  });
+}
 
 }
